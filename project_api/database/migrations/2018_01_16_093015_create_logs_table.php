c314370177f78c54c1716fca2efd7664061ca256
Dimitri Sandron <d.sandron@it-akademy.fr>
2018-01-16 14:47:27 +0100

    all logs tables in same migration file

--- project_api/database/migrations/2018_01_16_093015_create_logs_table.php
+++ project_api/database/migrations/2018_01_16_093015_create_logs_table.php
@@ -13,15 +13,33 @@ class CreateLogsTable extends Migration
      */
     public function up()
     {
-        Schema::table('logs', function (Blueprint $table) {
+        Schema::create('logs', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title', 20);
-            $table->string('status', 50);
-            $table->integer('project_id')->unsigned();
-            $table->foreign('project_id')->references('id')->on('projects');
-            $table->integer('log_type_id')->unsigned();
-            $table->foreign('log_type_id')->references('id')->on('log_types');
+            $table->string('status', 50)->nullable();
+            $table->integer('project_id')->unsigned()->nullable();
+            $table->integer('log_type_id')->unsigned()->nullable();
             $table->timestamps();
+        });
+        Schema::create('log_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type', 100);
+        });
+        Schema::create('log_lines', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->integer('log_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('logs', function(Blueprint $table)
+        {
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('set null');
+            $table->foreign('log_type_id')->references('id')->on('log_types')->onDelete('set null');
+        });
+        Schema::table('log_lines', function(Blueprint $table)
+        {
+            $table->foreign('log_id')->references('id')->on('logs')->onDelete('set null');
         });
     }
 
@@ -33,5 +51,7 @@ class CreateLogsTable extends Migration
     public function down()
     {
         Schema::dropIfExists('logs');
+        Schema::dropIfExists('log_types');
+        Schema::dropIfExists('log_lines');
     }
 }
