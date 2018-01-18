ebfedd3d1ce84897054ff15f1e3d20733a1b3b7a
Alexis Bussières <bussieres.alexis.dev@gmail.com>
2018-01-18 14:13:40 +0100

    Add route to analyze a project, provide class to run test

---
+++ project_api/app/Analyzer/AnalyzerToolInterface.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apprenant
+ * Date: 18/01/18
+ * Time: 10:11
+ */
+
+namespace App\Analyzer;
+
+
+interface AnalyzerToolInterface
+{
+    /**
+     * You need to follow this structure
+     *
+     * [
+     *      'your-command' => [
+     *          'arg1',
+     *          'arg2',
+     *      ]
+     * ]
+     *
+     * @return array
+     */
+    public function getCommand(): array;
+
+    /**
+     * @param string $output Set the plain output from command execution
+     * @return mixed
+     */
+    public function setOutput(string $output);
+
+    /**
+     * Return all formated lines as array
+     *
+     * @return array
+     */
+    public function getLines(): array;
+
+    /**
+     * Return if command is success or not
+     *
+     * @return bool
+     */
+    public function isSuccess(): bool;
+}
