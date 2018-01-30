83e36908e32544128adb51b3fc7d8832b0256cee
dimsand <d.sandron@it-akademy.fr>
2018-01-30 11:05:46 +0100

    add auth token api

--- project_api/routes/api.php
+++ project_api/routes/api.php
@@ -16,3 +16,16 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', 'API\PassportController@login');
+Route::post('register', 'API\PassportController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('get-details', 'API\PassportController@getDetails');
+
+    Route::post('project', 'AnalyzerController@request')->name('project_request');
+    Route::get('project', function (){
+        return response()->json(array('return_code'=>"FAILED", 'error'=>"Missing slug parameter."));
+    });
+    Route::get('project/{slug}', 'ProjectController@getLogs')->name('project_get_log');
+});
